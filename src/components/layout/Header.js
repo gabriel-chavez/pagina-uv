4d8d0c15373b6619b9b6e4f60aa67bfdc141fccd
UNIVIDA\lchavez <lchavez@univida.bo>
2025-01-05 10:52:23 -0400

    actualización header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -10,31 +10,6 @@ export default async function Header() {
   const menu = resultado.datos.sort((a, b) => a.orden - b.orden);
 
   return (
-<<<<<<< HEAD
-    <>
-      <header className="sticky-header">
-        <nav className="main-menu">
-          <div className="main-menu__wrapper">
-            <div className="main-menu__wrapper-inner">
-              <div className="main-menu__left">
-                <div className="main-menu__logo">
-                  <Link href="/">
-                    <img
-                      src="/assets/images/resources/logo-univida.png"
-                      alt="Logo Univida"
-                      style={{ height: '60px' }}
-                    />
-                  </Link>
-                </div>
-                <div className="main-menu__main-menu-box">
-                  <a href="#" className="mobile-nav__toggler">
-                    <i className="fa fa-bars"></i>
-                  </a>
-                  <ul className="main-menu__list">
-                    {renderMenu(menu)}
-                  </ul>
-                </div>
-=======
     <header className="sticky-header">
       <nav className="main-menu">
         <div className="main-menu__wrapper">
@@ -48,7 +23,6 @@ export default async function Header() {
                     style={{ height: '60px' }}
                   />
                 </Link>
->>>>>>> 02aeeb5f127e3951b6a1a79abee37b068bae1e50
               </div>
               <div className="main-menu__main-menu-box">
                 <MenuCliente menu={menu}  />
