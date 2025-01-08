8995605d7846916dfb4e7dafc2b56ccfc34e89cc
UNIVIDA\lchavez <lchavez@univida.bo>
2025-01-07 22:20:38 -0400

    actualización ssl

--- next.config.mjs
+++ next.config.mjs
@@ -5,38 +5,41 @@ const nextConfig = {
   reactStrictMode: true,
 
   eslint: {
-    ignoreDuringBuilds: true,  
+    ignoreDuringBuilds: true, 
   },
 
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
 
-  swcMinify: false,
+  swcMinify: true, 
   output: 'standalone',
 
   experimental: {
     incrementalCacheHandlerPath: false,
-    turbo: true,  
+    turbo: true, 
+    nextScriptWorkers: true, 
   },
 
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Evita minificación de jQuery (puede causar tiempos largos)
+  webpack: (config, { buildId, dev, isServer }) => {
+  
+    if (!dev) {
+      config.optimization.minimize = true;
+    }
+
+ 
     config.plugins.push(
       new webpack.ProvidePlugin({
         $: "jquery",
         jQuery: "jquery",
         "window.jQuery": "jquery",
       })
-    );
-
-    // Evita duplicar jQuery (en caso de builds largos)
+    ); 
     config.resolve.alias['jquery'] = 'jquery';
 
     return config;
   },
 
-  // Desactiva Telemetría de Next.js para ahorrar tiempo
   telemetry: false,
   async headers() {
     return [
